07d117427140a73f4085f5e0261c31270c417f4d
NikitaBorole <98250739+NikitaBorole@users.noreply.github.com>
2022-04-11 00:07:47 +0530

    Add files via upload

---
+++ que1_wr.c
@@ -0,0 +1,18 @@
+//Nikita Borole
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/shm.h>
+#include<string.h>
+int main()
+{
+	int i;
+	void *shared_memory;
+	char buff[100];
+	int shmid;
+	shmid=shmget((key_t)2345, 1024, 0666|IPC_CREAT); 
+	
+	shared_memory=shmat(shmid,NULL,0); 
+
+	printf("Memory allocated");
+}
